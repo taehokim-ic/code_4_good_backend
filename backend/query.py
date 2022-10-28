25b823e623f90db34c80e615a3982084f80d1505
Taeho Kim <tk3718@ic.ac.uk>
2022-10-28 21:50:30 +0100

    Added querying functions to get events

--- backend/query.py
+++ backend/query.py
@@ -1,14 +1,22 @@
+from audioop import add
 from db import engine
 from models.models import *
 from sqlmodel import Session, select, or_
 from pydantic import BaseModel
-
+from datetime import date
 
 class StudentData(BaseModel):
     user_name: str
     name: str
     mem_word: str
     
+    class Config:
+        orm_mode = True
+        
+class EventData(BaseModel):
+    name: str
+    date: datetime
+
     class Config:
         orm_mode = True
 
@@ -31,8 +39,9 @@ class StudentData(BaseModel):
 #             return []
 #         return [(row.keyword, row.link) for row in first]
 
+
 # Adds student to stuednt database, returns False if student exists    
-def register_student(student_data: StudentData, table=Student):
+def registered_student(student_data: StudentData, table=Student):
     student_exists = check_if_student_exists(student_data=student_data) 
     with Session(engine) as session:    
         if not student_exists:
@@ -58,7 +67,30 @@ def check_if_student_exists(student_data: StudentData, table=Student):
 
         return True
         
-if __name__ == "__main__":
-    new_student = StudentData(name="TD22Dho", user_name="23dwd22123U", mem_word="Taego")
-    register_student(new_student)
+# Getting events on or after a certain date
+def get_event_on_or_after_specific_date(date: datetime, table=Events):
+    with Session(engine) as session:
+        statement = select(table).where(table.date>=date)
+        result = session.exec(statement=statement).all()
+        
+        return result    
     
+# Getting events for a certain date
+def get_event_at_specific_date(date: datetime, table=Events):
+    with Session(engine) as session:
+        statement = select(table).where(table.date==date)
+        result = session.exec(statement=statement).all()
+        
+        return result
+    
+# Add event for specific date
+def add_event_at_specific_date(event_data: EventData, table=Events):
+    with Session(engine) as session:
+        session.add(table(name=event_data.name, 
+                          date=event_data.date))
+        session.commit()
+        
+if __name__ == "__main__":
+    new_student = EventData(name="TD22Dho", date=datetime.today())
+    add_event_at_specific_date(new_student)
+    print(get_event_on_or_after_specific_date(datetime.today()))
