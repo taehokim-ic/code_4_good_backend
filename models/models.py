e188b09b8d515b63d19a5363fa9b92520631286f
Taeho Kim <tk3718@ic.ac.uk>
2022-10-28 22:43:31 +0100

    Removed files from backend folder

---
+++ models/models.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from typing import Optional
+
+from sqlmodel import SQLModel, Field
+
+class Student(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True)
+    name: str = Field(index=True)
+    user_name: str
+    mem_word: str
+
+class Events(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True)
+    name: str = Field(index=True)
+    date: datetime
+    
+class Volunteer(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True)
+    name: str = Field(index=True)
+    date: datetime
+    
